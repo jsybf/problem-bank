e150f4048d7264e89746be7ce4e5a3e7530ff879
park jun hyoung <gitpdeathwa@yonsei.ac.kr>
2024-01-23 22:59:32 +0900

    implement entity and repository of problem

---
+++ src/main/java/gitp/problembank/domain/Problem.java
@@ -0,0 +1,49 @@
+package gitp.problembank.domain;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+
+@Node("problem")
+@Getter
+public class Problem {
+
+	@Id @GeneratedValue(UUIDStringGenerator.class)
+	private String id;
+
+	private String name;
+
+	private Long rdbmsId;
+
+	@Relationship(type = "related_problem")
+	private Set<Problem> relatedProblemSet = new HashSet<>();
+
+	@Relationship(type = "tagged")
+	private Set<SkillTag> skillTagSet = new HashSet<>();
+
+	public void addRelatedProblem(Problem... problems) {
+		relatedProblemSet.addAll(Arrays.asList(problems));
+		Arrays.asList(problems).forEach(problem -> problem.getRelatedProblemSet().add(this));
+	}
+
+	public void removeRelatedProblem(Problem problem) {
+		relatedProblemSet.removeIf(p -> p.getName().equals(problem.getName()));
+		problem.getRelatedProblemSet().removeIf(p -> p.getName().equals(this.getName()));
+	}
+
+	public Problem(String id, String name, Long rdbmsId) {
+		this.id = id;
+		this.name = name;
+		this.rdbmsId = rdbmsId;
+	}
+
+	public static Problem of(String name, Long rdbmsId) {
+		return new Problem(null, name, rdbmsId);
+	}
+}
