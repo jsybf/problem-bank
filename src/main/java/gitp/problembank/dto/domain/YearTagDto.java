7c4d6cc3e1bbe7128fee086c02e7879eba6442c5
park jun hyoung <gitpdeathwa@yonsei.ac.kr>
2024-01-24 14:14:12 +0900

    add dto except problemDto

---
+++ src/main/java/gitp/problembank/dto/domain/YearTagDto.java
@@ -0,0 +1,24 @@
+package gitp.problembank.dto.domain;
+
+import gitp.problembank.domain.SkillTag;
+import gitp.problembank.domain.YearTag;
+import java.time.Year;
+import lombok.Getter;
+
+@Getter
+public class YearTagDto {
+
+	private final Year year;
+
+	public YearTagDto(Year year) {
+		this.year = year;
+	}
+
+	public YearTag toEntity() {
+		return YearTag.of(year);
+	}
+
+	public static YearTagDto toDto(YearTag entity) {
+		return new YearTagDto(entity.getYear());
+	}
+}
